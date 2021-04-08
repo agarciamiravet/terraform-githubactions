f44e7207aaa7f6688dd7e62efaa60bc9bcac12d7
Alejandro Garcia Miravet <agarciamiravet@gmail.com>
2021-04-08 18:41:49 +0200

    Update heroes.tf

--- src/terraform/heroes.tf
+++ src/terraform/heroes.tf
@@ -106,13 +106,13 @@ resource "azurerm_sql_server" "sqlsrv" {
   administrator_login_password = var.sql_server_pwd
 }
 
-#resource "azurerm_sql_firewall_rule" "sqlsrvfirewall" {
-#  name                = "sqlfr1"
-#  resource_group_name = azurerm_resource_group.zerorg.name
-#  server_name         = azurerm_sql_server.sqlsrv.name
-#  start_ip_address    = "1.1.1.1"
-#  end_ip_address      = "255.255.255.255"
-#}
+resource "azurerm_sql_firewall_rule" "sqlsrvfirewall" {
+  name                = "sqlfr1"
+  resource_group_name = azurerm_resource_group.zerorg.name
+  server_name         = azurerm_sql_server.sqlsrv.name
+  start_ip_address    = "1.1.1.1"
+  end_ip_address      = "255.255.255.255"
+}
 
 resource "azurerm_sql_database" "heroesdatabase" {
   name                             = var.database_name
