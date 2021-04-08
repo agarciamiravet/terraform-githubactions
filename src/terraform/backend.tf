8ea6365a20b3ec9c5c31e130016e230bd5503a4c
Alejandro Garcia <alejandro.garcia@analyticalways.com>
2021-04-08 17:57:11 +0200

    ~change backend

--- src/terraform/backend.tf
+++ src/terraform/backend.tf
@@ -1,11 +1,10 @@
 terraform {
-  #backend "azurerm" {
- #  resource_group_name  = "rg-codemotion"
- #  storage_account_name = "stcodemotion"
- # container_name       = "tfstate"
- #  key                  = "terraform.remote.tfstate"
- #}
-  backend "local" {}
+backend "azurerm" {
+   resource_group_name  = "rg-codemotion"
+   storage_account_name = "stcodemotion"
+  container_name       = "tfstate"
+   key                  = "terraform.remote.tfstate"
+ }
 
   required_providers {
     azurerm = {
